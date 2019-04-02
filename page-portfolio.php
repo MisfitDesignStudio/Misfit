fd81c5b2cc39a2e4e71de055b81ab1d58ce5fa65
Mario <mario@misfitdesign.hr>
2019-04-02 23:53:58 +0200

    indentations

--- page-portfolio.php
+++ page-portfolio.php
@@ -11,17 +11,17 @@
 get_header();
 ?>
 <div class="container container--wide v-padding content-sidebar">
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-      <section class="portfolio-page">
+			<section class="portfolio-page">
 
-        <h1 class="portfolio-page__title"><?php the_title(); ?></h1>
+				<h1 class="portfolio-page__title"><?php the_title(); ?></h1>
 
-        <div class="card-grid card-grid--portfolio">
+				<div class="card-grid card-grid--portfolio">
 
-          <!-- Custom Query -->
-          <?php
+					<!-- Custom Query -->
+					<?php
         // query arguments 
         $args = array(
           'post_type' => 'project',
@@ -37,27 +37,27 @@ get_header();
         while($portfolioPosts -> have_posts()) : $portfolioPosts -> the_post();
         ?>
 
-          <div class="portfolio-item">
-            <img class="portfolio-item__image" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
-            <div class="portfolio-item__overlay">
-              <a href="<?php the_permalink(); ?>" class="btn btn--small btn--primary"><?php the_title(); ?></a>
-            </div>
-          </div>
+					<div class="portfolio-item">
+						<img class="portfolio-item__image" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+						<div class="portfolio-item__overlay">
+							<a href="<?php the_permalink(); ?>" class="btn btn--small btn--primary"><?php the_title(); ?></a>
+						</div>
+					</div>
 
-          <?php 
+					<?php 
         //restore original post data
         wp_reset_postdata();
 
         endwhile;  ?>
-        </div>
+				</div>
 
-      </section>
+			</section>
 
 
-    </main><!-- #main -->
-  </div><!-- #primary -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
-  <?php
+	<?php
 get_sidebar(); ?>
 </div>
 <?php get_footer();
--- single-project.php
+++ single-project.php
@@ -15,36 +15,37 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-    <section class="project-page">
+			<section class="project-page">
 
-      <?php while (have_posts()) : the_post(); ?>
+				<?php while (have_posts()) : the_post(); ?>
 
-        <h1 class="project-page__title"><?php the_title(); ?></h1>
-        
-        <div class="project-page__meta">          
-        <h3 class="project-page__meta__client"><?php the_field('client_name'); ?></h3>
-        <ul class="project-page__meta__category-list">
-          <?php
-          foreach (get_categories() as $category){
-            echo "<li>";
-            echo $category->name;
-            echo "</li>";
-          } ?>
-        </ul>
-        </div>
+				<h1 class="project-page__title"><?php the_title(); ?></h1>
 
-        <?php the_content(); ?>
+				<div class="project-page__meta">
+					<h3 class="project-page__meta__client"><?php the_field('client_name'); ?></h3>
+					<ul class="project-page__meta__category-list">
+						<?php
+							foreach (get_categories() as $category) {
+								echo "<li>";
+								echo $category->name;
+								echo "</li>";
+							} ?>
+					</ul>
+				</div>
 
-        <a href="<?php echo get_home_url(); ?>/portfolio" class="btn btn--medium btn--secondary btn--back">See all projects</a>
+				<?php the_content(); ?>
 
-      <?php endwhile; ?>
+				<a href="<?php echo get_home_url(); ?>/portfolio" class="btn btn--medium btn--secondary btn--back">See all
+					projects</a>
 
-    </section>
+				<?php endwhile; ?>
+
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-get_sidebar('portfolio'); ?>
+	<?php
+	get_sidebar('portfolio'); ?>
 </div>
 <?php get_footer();
