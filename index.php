7de8c16e2cc60c2f824309c7e1ce6422f69d185f
Mario <mario@misfitdesign.hr>
2019-04-07 18:08:31 +0200

    blog grid prepared

--- index.php
+++ index.php
@@ -14,52 +14,41 @@
 
 get_header();
 ?>
-	<div class="container v-padding content-sidebar">
+<div class="container v-padding content-sidebar">
 	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
+			<?php	if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
+				if ( is_home() && ! is_front_page() ) : ?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+				<?php endif; ?>
+
+				<div class="grid-blog">
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+
+						get_template_part( 'template-parts/content', 'blog' );
+
+					endwhile; ?>
+				</div> <!-- end blog grid-->
+
+				<?php	the_posts_navigation();
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
 			endif; ?>
 
-			<div class="card-grid card-grid--blog">
-
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post();
-
-					/*
-					* Include the Post-Type-specific template for the content.
-					* If you want to override this in a child theme, then include a file
-					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					*/
-					get_template_part( 'template-parts/content', 'blog' );
-
-				endwhile; ?>
-
-			</div>
-
-			<?php
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
+		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-get_sidebar(); ?>
+	<?php get_sidebar(); ?>
 </div>
 <?php
 get_footer();
