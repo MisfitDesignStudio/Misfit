5f42a6f2d1506cbe42f9f00c0669353c40aabe0e
Mario <mario@misfitdesign.hr>
2019-05-12 23:54:51 +0200

    single blog restyled

--- single.php
+++ single.php
@@ -9,36 +9,29 @@
 
 get_header();
 ?>
-<div class="container v-padding content-sidebar">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div class="container v-padding">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+	<?php
+	while (have_posts()) :
+		the_post();
 
-			get_template_part( 'template-parts/content', 'blog-single' );
+		get_template_part('template-parts/content', 'blog-single');
 
-			the_post_navigation(
-				array(
-					'prev_text' => __('<i class="fas fa-caret-left"></i> Older post', 'misfitdesign'),
-					'next_text' => __('Newer post <i class="fas fa-caret-right"></i>', 'misfitdesign'),
-					'screen_reader_text' => __('Posts navigation', 'misfitdesign')
-				)
-			);
+		the_post_navigation(
+			array(
+				'prev_text' => __('<i class="fas fa-caret-left"></i> Older post', 'misfitdesign'),
+				'next_text' => __('Newer post <i class="fas fa-caret-right"></i>', 'misfitdesign'),
+				'screen_reader_text' => __('Posts navigation', 'misfitdesign')
+			)
+		);
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		// If comments are open or we have at least one comment, load up the comment template.
+		if (comments_open() || get_comments_number()) :
+			comments_template();
+		endif;
 
-		endwhile; // End of the loop.
-		?>
+	endwhile; // End of the loop.
+	?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar(); ?>
 </div>
 <?php get_footer();
--- template-parts/content-blog-single.php
+++ template-parts/content-blog-single.php
@@ -1,58 +1,27 @@
 <?php
 /**
- * Template part for displaying blog posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
  * @package MisfitDesign
  */
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('single-blog-post'); ?>>
+<article class="single-blog-post">
 
-  <div class="single-blog-post__image">
-		<?php if(has_post_thumbnail()) : ?>
-		 <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=" ">
+	<h1 class="single-blog-post__title"><?php the_title(); ?></h1>
+
+	<small class="single-blog-post__meta">Posted by <?php the_author(); ?> on <?php the_date(); ?> in: <span><?php the_category(', '); ?></span></small>
+
+
+	<div class="single-blog-post__hero">
+		<?php if (has_post_thumbnail()) : ?>
+			<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=" ">
 		<?php else : ?>
 			<img src="<?php echo get_template_directory_uri(); ?>/img/blog-post.jpg" alt="">
 		<?php endif; ?>
-    
-  </div>
-
-	<header class="single-blog-post__header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="single-blog-post__title">', '</h1>' );
-		else :
-			the_title( '<h2 class="single-blog-post__title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="single-blog-post__meta">
-				<?php
-				misfitdesign_posted_on();
-				misfitdesign_posted_by();
-				?>
-			</div><!-- .single-blog-post__meta -->
-		<?php endif; ?>
-	</header><!-- .single-blog-post__header -->
-
-	
+	</div>
 
 	<div class="single-blog-post__content">
-		<?php
-    the_content();
+		<?php the_content(); ?>
+	</div>
 
-		// wp_link_pages( array(
-		// 	'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'misfitdesign' ),
-		// 	'after'  => '</div>',
-		// ) );
-		?>
-	</div><!-- .single-blog-post__content -->
-
-	<footer class="single-blog-post__footer">
-		<?php misfitdesign_entry_footer(); ?>
-	</footer><!-- .single-blog-post__footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+</article>
