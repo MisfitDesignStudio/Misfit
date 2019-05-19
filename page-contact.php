37c2aadb7b406830e1085d2b0e4d2d6dbcb55b8d
Mario <mario@misfitdesign.hr>
2019-05-19 15:01:04 +0200

    contact page done

--- page-contact.php
+++ page-contact.php
@@ -10,39 +10,27 @@
 
 get_header();
 ?>
-	<div class="container container--wide v-padding">
 
+<div class="page-split">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main contact-wrapper">
+	<div class="page-split__image">
+		<img src="<?php echo get_template_directory_uri(); ?>/img/contact-bg.svg" alt="">
+	</div>
 
-		<div class="contact-info">
-			<h2 class="contact-info__title">Interested in working together?</h2>
-			<p class="contact-info__phone">+34 684 188 301</p>
-			<a class="contact-info__email" href="mailto:mario@misfitdesign.hr">mario@misfitdesign.hr</a>
-			<p class="contact-info__address">07800, Ibiza</p>
-			<h2 class="contact-info__subtitle">Let's connect!</h2>
-			<ul class="contact-info__social-links">
-				<li><a href="https://www.facebook.com/misfitdsgn"><i class="fab fa-facebook"></i></a></li>
-				<li><a href="https://www.instagram.com/misfit.design"><i class="fab fa-instagram"></i></a></li>
-				<li><a href="https://dribbble.com/misfit_design"><i class="fab fa-dribbble"></i></a></li>
-			</ul>
+	<div class="page-split__form">
+		<h1 class="page-title">Let's talk!</h1>
+		<p class="page-subtitle">Get in touch for a quote, help or just to say hi!</p>
+		<div class="contact-page__social-buttons">
+			<a href="https://www.facebook.com/misfitdsgn" class="contact__social__link"><i class="fab fa-facebook"></i></a>
+			<a href="https://www.instagram.com/misfit.design" class="contact__social__link"><i class="fab fa-instagram"></i></a>
+			<a href="https://dribbble.com/misfit_design" class="contact__social__link"><i class="fab fa-dribbble"></i></a>
 		</div>
+		<div class="contact-page__form">
+			<?php echo do_shortcode('[contact-form-7 id="124" title="Main Contact Form"]'); ?>
+		</div>
+	</div>
 
-		<div class="contact-form">
-		<h2 class="contact-info__title">Let's talk!</h2>
-			<?php
-				while ( have_posts() ) :
-					the_post();
-
-					the_content();
-
-				endwhile; // End of the loop.
-			?>		
-		</div>		
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</div>
 
 <?php
 
