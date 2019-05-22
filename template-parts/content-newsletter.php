53c3050f7bee1cbf3e6667b1421e0aca50f0a2da
Mario <mario@misfitdesign.hr>
2019-05-22 11:59:45 +0200

    mester

--- template-parts/content-newsletter.php
+++ template-parts/content-newsletter.php
@@ -1,10 +1,7 @@
-<section class="drop-shadow">
+<section class=" newsletter-section drop-shadow">
   <div class="container v-padding">
     <div id="mc_embed_signup">
-      <form
-        action="https://misfitdesign.us14.list-manage.com/subscribe/post?u=31efebe0ae18eb0e569bab12d&amp;id=4a804bf53e"
-        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate newsletter" target="_blank"
-        novalidate>
+      <form action="https://misfitdesign.us14.list-manage.com/subscribe/post?u=31efebe0ae18eb0e569bab12d&amp;id=4a804bf53e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate newsletter" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll" class="newsletter__inner">
 
           <div class="newsletter__content">
@@ -21,15 +18,14 @@
             <div class="response" id="mce-success-response" style="display:none"></div>
           </div>
           <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
-              name="b_31efebe0ae18eb0e569bab12d_4a804bf53e" tabindex="-1" value=""></div>
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_31efebe0ae18eb0e569bab12d_4a804bf53e" tabindex="-1" value=""></div>
           <div class="clear"></div>
         </div>
       </form>
     </div>
     <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
     <script type='text/javascript'>
-      (function ($) {
+      (function($) {
         window.fnames = new Array();
         window.ftypes = new Array();
         fnames[0] = 'EMAIL';
